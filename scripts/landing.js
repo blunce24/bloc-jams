3ff502e4f56f560c59e9900056f6d178ce059038
blunce24 <blairlunceford@gmail.com>
2016-09-18 21:24:14 -0500

    Assignment 8 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,16 @@
 var animatePoints = function() {
     var points = document.getElementsByClassName('point');
             
-    var revealFirstPoint = function() {
-            points[0].style.opacity = 1;
-            points[0].style.transform = "scaleX(1) translateY(0)";
-            points[0].style.mstransform = "scaleX(1) translateY(0)";
-            points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-            
-    var revealSecondPoint = function() {
-            points[1].style.opacity = 1;
-            points[1].style.transform = "scaleX(1) translateY(0)";
-            points[1].style.mstransform = "scaleX(1) translateY(0)";
-            points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-            
-    var revealThirdPoint = function() {
-            points[2].style.opacity = 1;
-            points[2].style.transform = "scaleX(1) translateY(0)";
-            points[2].style.mstransform = "scaleX(1) translateY(0)";
-            points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-            
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+    var revealPoint = function(i) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.mstransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+    
+    for (var j = 0; j < points.length; j++) {
+        revealPoint(j);
+    }
                 
 };
-            
             
