e190288324ff276c3a9e13480dad622bb010efc5
blunce24 <blairlunceford@gmail.com>
2016-09-20 21:36:05 -0500

    Assignment 9 Events Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.mstransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function(points) {
           
-    var revealPoint = function(i) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.mstransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-    
-    for (var j = 0; j < points.length; j++) {
-        revealPoint(j);
-    }
+    forEach(points, revealPoint);
                 
 };
             
