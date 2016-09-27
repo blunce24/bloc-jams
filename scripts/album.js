798750b47ba0421a68cd0de744cc327e7383b596
blunce24 <blairlunceford@gmail.com>
2016-09-26 21:14:59 -0500

    Checkpoint 20 Completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,23 @@
 var setSong = function(songNumber) {
+    if (currentSoundFile) {
+        currentSoundFile.stop();
+    }
+    
     currentlyPlayingSongNumber = parseInt(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+        formats: ['mp3'],
+        preload: true
+    });
+    
+    setVolume(currentVolume);
+};
+
+var setVolume = function(volume) {
+    if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+    }
 };
 
 var getSongNumberCell = function(number) {
@@ -28,12 +45,18 @@ var createSongRow = function(songNumber, songName, songLength) {
         if (currentlyPlayingSongNumber !== songNumber) {
             $(this).html(pauseButtonTemplate);
             setSong(songNumber);
+            currentSoundFile.play();
             updatePlayerBarSong();
         } else if (currentlyPlayingSongNumber === songNumber) {
-            $(this).html(playButtonTemplate);
-            $('.main-controls .play-pause').html(playerBarPlayButton);
-            currentlyPlayingSongNumber = null;
-            currentSongFromAlbum = null;
+            if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton); 
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton); 
+                currentSoundFile.pause();
+            }
         } 
         
      }; 
@@ -126,6 +149,8 @@ var nextSong = function() {
     // set a new CurrentlyPlayingSongNumber and currentSongFromAlbum
     setSong(index + 1);
     
+    currentSoundFile.play();
+    
     // update player bar
     updatePlayerBarSong();
     
@@ -159,6 +184,8 @@ var previousSong = function() {
     }
 
     setSong(index + 1);
+    
+    currentSoundFile.play();
 
     updatePlayerBarSong();
     
@@ -181,6 +208,8 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
