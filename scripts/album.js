fe3db1534f3d69a63610ad0deebdef7bed6b1237
blunce24 <blairlunceford@gmail.com>
2016-09-26 21:24:29 -0500

    Checkpoint 20 Completed

--- scripts/album.js
+++ scripts/album.js
@@ -198,6 +198,19 @@ var previousSong = function() {
     
 };
 
+var togglePlayFromPlayerBar = function() {
+    if (currentSoundFile.isPaused()) {
+        var songNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
+        songNumberCell.html(pauseButtonTemplate);
+        $(this).html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else {
+        var songNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
+        songNumberCell.html(playButtonTemplate);
+        $(this).html(playerBarPlayButton);
+        currentSoundFile.pause();
+    }
+};
 
 // album button
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
@@ -213,9 +226,11 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
  $(document).ready(function() {
      setCurrentAlbum(albumPicasso);  
      $previousButton.click(previousSong);
      $nextButton.click(nextSong);
+     $playPauseButton.click(togglePlayFromPlayerBar);
  });
